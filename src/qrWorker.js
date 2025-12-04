5e8f5568446fa93f8476c13d8f8b735e886cfa67
SAMCR4 <salseytoe@atomicmail.io>
2025-12-04 14:52:50 -0600

    Delete src directory

--- src/qrWorker.js
+++
@@ -1,8 +0,0 @@
-
-importScripts('/src/lib/qr/jsQR.js');
-
-onmessage = function(e){
-  const {data,width,height} = e.data;
-  const code = jsQR(data, width, height, {inversionAttempts:"attemptBoth"});
-  postMessage(code ? {data:code.data} : null);
-};
