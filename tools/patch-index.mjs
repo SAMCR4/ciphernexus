7a90f4e777e2ebe3f7dbd4e705439c5f1aa6516b
SAMCR4 <salseytoe@atomicmail.io>
2025-12-04 14:38:32 -0600

    Delete tools/patch-index.mjs

--- tools/patch-index.mjs
+++
@@ -1,6 +0,0 @@
-
-import fs from "fs";
-let html = fs.readFileSync("index.html", "utf8");
-html = html.replace(/src="\.\/src\/main.*?"/, 'src="./src/main.min.js"');
-fs.writeFileSync("index.html", html);
-console.log("✔ index.html updated");
